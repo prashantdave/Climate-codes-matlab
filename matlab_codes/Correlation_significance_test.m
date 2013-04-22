9f5c3331aaaa175587342c0d28811c2f3a790b00
prashantdave <prashantdave25@gmail.com>
2013-04-22 16:11:21 +0530

    Added code for spatial correalation and hypothesis testing

--- matlab_codes/Correlation_significance_test.m
+++ matlab_codes/Correlation_significance_test.m
@@ -1,6 +1,8 @@
+function Correlation_significance_test(Threshold) 
+
 a = {'01','activ','aeronm','conv','diag','forcing','inputm','nudg','radm','tarcerm','xtsurf'};
 counter=0;
-for i=1:length(a);    
+for i=1:length(a)    
     dir_path = cell2mat(strcat('/home/prashant/Desktop/nc_data/ECHAM-HAM/2001/',a(i),'/0609/mat_files'));
     mat_files_path=cell2mat(strcat('/home/prashant/Desktop/nc_data/ECHAM-HAM/2001/',a(i),'/0609/mat_files/*.mat'));
     significant_variables_path = cell2mat(strcat('/home/prashant/Desktop/nc_data/ECHAM-HAM/2001/',a(i),'/0609/mat_files/significant_variables'));
@@ -13,7 +15,7 @@ for i=1:length(a);
     
             n = 420;
             alpha = 0.05;
-            Threshold = 0.7 %Corrleation Coefficient value; H_0 >= 0.7 (null hypothesis) H_a < 0.7 (alternate hypothesis).
+%            Threshold = 0.7 %Corrleation Coefficient value; H_0 >= 0.7 (null hypothesis) H_a < 0.7 (alternate hypothesis).
     
             % Fishers Z-transformation              
             Zr = atanh(corr_coeff_0);
---
+++ matlab_codes/Spatial_Correlation_significance_test.m
@@ -0,0 +1,35 @@
+function Spatial_Correlation_significance_test(Threshold) 
+
+a = {'01','activ','aeronm','conv','diag','forcing','inputm','nudg','radm','tarcerm','xtsurf'};
+counter=0;
+for i=1:length(a)
+    
+    dir_path = cell2mat(strcat('/home/prashant/Desktop/nc_data/ECHAM-HAM/2001/',a(i),'/0609/mat_files'));
+    mat_files_path=cell2mat(strcat('/home/prashant/Desktop/nc_data/ECHAM-HAM/2001/',a(i),'/0609/mat_files/*.mat'))
+    significant_variables_path = cell2mat(strcat('/home/prashant/Desktop/nc_data/ECHAM-HAM/2001/',a(i),'/0609/mat_files/significant_variables'));
+    files = dir(mat_files_path);
+
+    for l = 1:length(files)
+            s1 = files(l).name
+            s = sprintf('%s/',dir_path,s1);
+            load (s(1:end-1));
+    
+            alpha = 0.05;
+%            Threshold = 0.7 %Corrleation Coefficient value; H_0 >= 0.7 (null hypothesis) H_a < 0.7 (alternate hypothesis).
+             n = 667;
+             % Fishers Z-transformation              
+             Zr = atanh(abs(corr_coeff));
+             Z_rho = atanh(Threshold);
+             sigma_z = 1/sqrt(n-3);
+             z = (Zr-Z_rho)/sigma_z;
+             P = normcdf(z)
+             
+              if (P >= alpha)
+                  s2=sprintf('%s/',significant_variables_path,s1(1:end-4));
+                  s_mat = sprintf('%s', s2(1:end-1),'.mat');      
+                  save(s_mat);
+                  counter = counter+1
+              end
+            counter
+    end
+end
